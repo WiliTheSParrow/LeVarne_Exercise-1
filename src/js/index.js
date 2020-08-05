85fb6a1bcd9c50fa1c1790c30e76bc400882d5aa
Júlia W <w.julia91@gmail.com>
2020-08-05 15:50:18 +0200

    fetch data from local json

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,36 @@
 new Vue({
     el: '#app',
     data: {
-        message: 'Hello World!'
+        message: 'Hello World!',
+        allRelationships: [],
+        filteredRelationships: []
+    },
+
+    created: function () {
+        this.getLocalData('./data.json', 'allRelationships');
+
+    },
+
+    methods: {
+        // Fetching data from local json file and error handling.
+        getLocalData: function (link, destination) {
+            fetch(link, {
+                    method: "GET",
+                })
+                .then(response => response.json())
+                .then(json => {
+                    app[destination] = json;
+                    if (app.filteredRelationships == '') {
+                        this.getAllRelationships();
+                    };
+                })
+                .catch(error => error);
+        },
+
+        // Fills the filteredRelationships array with all relations.
+        getAllRelationships: function () {
+            this.filteredRelationships = this.allRelationships;
+        },
+
     }
 })
